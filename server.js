d9b45cfd14a48d6013ff14e9f413e59d32b68446
Filipa Rocha <pipa_sr@hotmail.com>
2025-03-12 15:59:51 +0000

    update server

--- server.js
+++ server.js
@@ -27,5 +27,4 @@ app.get('*', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
-    open(`http://localhost:${PORT}`); // Automatically open the browser
 });
