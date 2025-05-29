cdc60da5dd712a97ed42c9ed4deec686e7a66190
Filipa Rocha <pipa_sr@hotmail.com>
2025-05-29 12:41:05 +0100

    Update constants.js

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -23,7 +23,7 @@ export const TEAM_MAIL = ['nunojnunes@tecnico.ulisboa.pt', 'hugo.nicolau@tecnico
     'ana.gfo.henriques@campus.ul.pt','hugalexsimon@gmail.com', 'chiara.ceccarini6@unibo.it', 'ian.r.oakley@gmail.com', 'doga@adobe.com', 'soraiafpaulo@tecnico.ulisboa.pt', 'catia.prandi@unibo.it',
     'carine.lallemand@hotmail.fr', 'msgdionisio@gmail.com', 'imshuhao.ma@gmail.com', 'ian.r.oakley@gmail.com', 'catia.prandi@unibo.it', 'lizette.reitsma@mau.se', 'elise.vandenHoven@uts.edu.au', 'ellen.do@colorado.edu'];
 
-export const DISCOUNT_MAIL = ['terhim@gmail.com', 'marina.otero@gmail.com', 'xpux@pratt.edu', '1424996982@qq.com', 'k.zolotovsky@northeastern.edu', 'ja@jamieallen.com', 'esteban.agosinotero@stonybrook.edu'];
+export const DISCOUNT_MAIL = ['terhim@gmail.com', 'marina.otero@gmail.com', 'xpux@pratt.edu', '1424996982@qq.com', 'k.zolotovsky@northeastern.edu', 'ja@jamieallen.com', 'esteban.agosinotero@stonybrook.edu', 'a.mohindra.design@gmail.com'];
 
 export const WORKSHOP_MAIL = ['beatrizseveres@tecnico.ulisboa.pt','catia.prandi@unibo.it','rosan.chow@oth-regensburg.de','sjang310@gatech.edu','b.l.lee@exeter.ac.uk','david.goedicke@uni-due.de',
     'maria.v.zeller@inesctec.pt','wiva@itu.dk','s.ppali@cyens.org.cy','beatrice.vincenzi@bcu.ac.uk','lnjchonolulu@gmail.com','daniel.snow@ucdconnect.ie','maria.montoyavega@monash.edu',
