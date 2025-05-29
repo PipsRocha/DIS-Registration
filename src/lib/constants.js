e4fc8c7e2884edeacff58f77cd4f2faac5121244
Filipa Rocha <pipa_sr@hotmail.com>
2025-05-29 11:56:57 +0100

    Update constants.js

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -26,7 +26,7 @@ export const TEAM_MAIL = ['nunojnunes@tecnico.ulisboa.pt', 'hugo.nicolau@tecnico
 export const DISCOUNT_MAIL = ['terhim@gmail.com', 'marina.otero@gmail.com', 'xpux@pratt.edu', '1424996982@qq.com', 'k.zolotovsky@northeastern.edu', 'ja@jamieallen.com', 'esteban.agosinotero@stonybrook.edu'];
 
 export const WORKSHOP_MAIL = ['beatrizseveres@tecnico.ulisboa.pt','catia.prandi@unibo.it','rosan.chow@oth-regensburg.de','sjang310@gatech.edu','b.l.lee@exeter.ac.uk','david.goedicke@uni-due.de',
-    'maria.v.zeller@inesctec.pt','wiva@itu.dk','akitson@sfu.ca','s.ppali@cyens.org.cy','beatrice.vincenzi@bcu.ac.uk','lnjchonolulu@gmail.com','daniel.snow@ucdconnect.ie','maria.montoyavega@monash.edu',
-    'lauren_thu@sfu.ca','hitesh.dhiman@th-owl.de', 'eileen.wemmer@th-owl.de','j.avila@nottingham.ac.uk'];
+    'maria.v.zeller@inesctec.pt','wiva@itu.dk','s.ppali@cyens.org.cy','beatrice.vincenzi@bcu.ac.uk','lnjchonolulu@gmail.com','daniel.snow@ucdconnect.ie','maria.montoyavega@monash.edu',
+    'lauren_thu@sfu.ca','hitesh.dhiman@th-owl.de', 'eileen.wemmer@th-owl.de','j.avila@nottingham.ac.uk', 'mark.blythe@northumbria.ac.uk'];
 
 export const VALID_CODES = ['WKVYL1260','WHLYC3715','W9CRA4228','W8OF14255','WEERV5495','WOPZT5994','WAFVT6665','WH2UB6952','WFJMV8727','WAUPG9005','W2XWG9059','WCGJ69650','W6JXI9731','WUMJU9838','WAR9O3859','WJGUU1562','W836W1877'];
