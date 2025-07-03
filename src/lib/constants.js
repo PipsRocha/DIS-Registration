d8d47d8857a7cd82909e15be189c63be67712799
Filipa Rocha <PipsRocha@users.noreply.github.com>
2025-07-03 15:20:34 +0100

    Update team

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -13,21 +13,17 @@ export const CATEGORY_I = ['Afghanistan', 'Algeria', 'Angola', 'Bangladesh', 'Be
     'Samoa', 'Sao Tome and Principe', 'Senegal', 'Sierra Leone', 'Sri Lanka', 'Solomon Islands', 'Somalia', 'South Sudan', 'Sudan', 'Syria', 'Tadzhikistan', 'Tanzania',
     'Timor-Leste', 'Togo', 'Tunisia', 'Uganda', 'Ukraine', 'Uzbekistan', 'Vanuatu', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe'];
 
-export const SV_MAIL = ['soraya.anvari@dal.ca','yliu3494@gatech.edu', 'michelemarks3@gmail.com', 'fabiodealmeida9@gmail.com'];
+export const SV_MAIL = ['annajiskrova0@gmail.com'];
 
-export const TEAM_MAIL = ['sebastian.prost@citystgeorges.ac.uk', 'marianapestana@tecnico.ulisboa.pt',
-                          'nmc@fct.unl.pt','acoelho@fe.up.pt','frederico.duarte@tecnico.ulisboa.pt','pedro.galvao.ferreira@tecnico.ulisboa.pt','cwieczor3@gatech.edu',
-                          'gel@nus.edu.sg','filipa.rocha@tecnico.ulisboa.pt','salaimo@uoregon.edu', 'merrie@google.com', 'andre@dafne.pt', 'imshuhao.ma@gmail.com','paulo.bala@tecnico.ulisboa.pt',
-                          'mara.dionisio@staff.uma.pt', 'cintia.franca@staff.uma.pt', 
-                          'filipa.rocha@tecnico.ulisboa.pt','hugalexsimon@gmail.com', 'chiara.ceccarini6@unibo.it', 'manuel.biscoito@funchal.pt', 'echeon@syr.edu'];
+export const TEAM_MAIL = [];
 
 export const DISCOUNT_MAIL = ['jamie.allen@fhnw.ch', 'marina.otero@gmail.com','xpux@pratt.edu','1424996982@qq.com', 'k.zolotovsky@northeastern.edu', 'esteban.agosinotero@stonybrook.edu', 'avantikavelho1@gmail.com'];
 
 export const WORKSHOP_MAIL = ['b.l.lee@exeter.ac.uk','david.goedicke@uni-due.de','daniel.snow@ucdconnect.ie',
-     'j.avila@nottingham.ac.uk', 'mark.blythe@northumbria.ac.uk', 'yatingdai@xlab.iii.u-tokyo.ac.jp', 'luis.ferreira@arditi.pt'];
+     'j.avila@nottingham.ac.uk', 'mark.blythe@northumbria.ac.uk', 'yatingdai@xlab.iii.u-tokyo.ac.jp'];
 
 export const VALID_CODES = ['WKVYL1260','WHLYC3715','W9CRA4228','W8OF14255','WEERV5495','WOPZT5994','WAFVT6665','WH2UB6952','WFJMV8727','W3','W2XWG9059','WCGJ69650','W6JXI9731','WUMJU9838','WAR9O3859','WJGUU1562','W836W1877', 'DCO'];
 
-export const EB_MAIL=['pan.meng@northeastern.edu', 'kwangyoung@kaist.ac.kr', 'frochaix@gsu.edu'];
+export const EB_MAIL=['frochaix@gsu.edu'];
 
-export const EBW_MAIL=['cristina.gena@unito.it'];
+export const EBW_MAIL=[];
